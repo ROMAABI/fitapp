c52401254592db72992eec87be3cced8dadf3fab
ROMAABI <abimanyuannadhurai18@gmail.com>
2025-12-23 09:24:46 +0530

    mistake

---
+++ python exercise/list.py
@@ -0,0 +1,3 @@
+# this is just exa,ple of the list#
+a=[1,2,3,4,5,6]
+print(a)
---
+++ python exercise/pythone if lese demo 2.py
@@ -0,0 +1,7 @@
+mark=int (input("result:"))
+if (mark==35):
+    print("pass")
+if (mark >= 35):
+    print("mark is:", mark, "pass")
+else:
+    print("fail")
